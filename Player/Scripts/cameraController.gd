1d6802cf187c0889d85ffb64cfc150ef40f5497b
Skiviper-AJM <adam.mcardlesep13@gmail.com>
2024-12-07 11:23:49 +1300

    Added a default zoom level export

--- Player/Scripts/cameraController.gd
+++ Player/Scripts/cameraController.gd
@@ -11,15 +11,17 @@ var v_sensitivity: float = 0.01
 var h_acceleration: float = 10.0
 var v_acceleration: float = 10.0
 
+
 @export var cam_max_zoom = -5 #maximum zoom in value
 @export var cam_min_zoom = -10 #controls max distance of cam to player - is also the default zoom
-@onready var current_zoom = cam_min_zoom #logs the 'current' zoom value to the minimum, which is also the default, on start
+@export var cam_default_zoom = -6
+@onready var current_zoom = cam_default_zoom #logs the 'current' zoom value to the minimum, which is also the default, on start
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_node("h/v").spring_length = cam_min_zoom #sets the spring length to the minimum zoom level at startup
+	get_node("h/v").spring_length = cam_default_zoom #sets the spring length to the minimum zoom level at startup
 	
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) #captures future mouse inputs
 
@@ -31,10 +33,10 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if Input.is_action_pressed("CameraZoomIn"):
 			current_zoom += 1 #zoom in
-			#print("Zoom is at" + str(current_zoom))
+			print("Zoom is at" + str(current_zoom))
 		elif Input.is_action_pressed("CameraZoomOut"):
 			current_zoom -= 1 #zoom out
-			#print("Zoom is at" + str(current_zoom))
+			print("Zoom is at" + str(current_zoom))
 	
 	
 
