366dad4970d9d9f06c8e77eb4ef03ab386b435f3
Skiviper-AJM <adam.mcardlesep13@gmail.com>
2024-12-09 03:24:48 +1300

    Item Sorting

--- UI/Scripts/item_data.gd
+++ UI/Scripts/item_data.gd
@@ -1,12 +1,13 @@
 class_name ItemData
 extends Resource
 
-enum Type{WEAPON, MISC, MAIN, CONSUMEABLE, SHIELD, HELMET}
+enum Type{WEAPON, MISC, MAIN, HPOTION, MPOTION, SHIELD, HELMET, PANTS, BOOTS, ARMOR}
 @export var type: Type
 @export var item_name: String
 @export var item_damage: int
 @export var item_defense: int
 @export var item_health: int
+@export var item_mana: int
 @export var stackable: bool
 @export var count: int
 @export_multiline var description: String
