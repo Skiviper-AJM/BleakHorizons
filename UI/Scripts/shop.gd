754f3c2cf9312556ad5796492791f29c7694fa0f
Skiviper-AJM <adam.mcardlesep13@gmail.com>
2024-12-09 06:20:35 +1300

    Shop is functional

--- UI/Scripts/shop.gd
+++ UI/Scripts/shop.gd
@@ -4,11 +4,20 @@ extends CanvasLayer
 @onready var shop_item_scene:PackedScene = preload("res://UI/Scenes/shop_item.tscn")
 var current_item: ItemData
 
+func _ready() -> void:
+	for i in PlayerData.items: 
+		if i != "default":
+			var shop_item_temp = shop_item_scene.instantiate()
+			shop_item_temp.item_info = PlayerData.items[i]
+			shop_item_temp.get_node("image").texture = PlayerData.items[i].item_texture
+			get_node("shop_items").add_child(shop_item_temp)
+	get_node("item_info").hide()
 
 func _on_buy_pressed():
-	pass # Replace with function body.
+	get_node("../container/Inventory").add_item(str(current_item.item_name))
 
 
 func _on_close_pressed():
-	pass # Replace with function body.
+	get_tree().paused = false
+	self.hide()
 
