5e9e5bce7b8f55e2d7f250cd5ae207ff88deefdf
Skiviper-AJM <adam.mcardlesep13@gmail.com>
2024-12-08 23:34:11 +1300

    Basic State machine controller
    Enemies have to be modular and scaleable, unlike the player, so a script based state machine is required

--- Enemies/Scripts/AI Melee/IdleState.gd
+++ Enemies/Scripts/AI Melee/IdleState.gd
@@ -1,2 +1,3 @@
 extends Node
-class_name IdleState
+
+
--- Enemies/Scripts/AI Melee/StateMachine.gd
+++ Enemies/Scripts/AI Melee/StateMachine.gd
@@ -1,1 +1,17 @@
 extends Node
+
+var state = {
+	"Idle": preload("res://Enemies/Scripts/AI Melee/IdleState.gd"),
+	"Run": preload("res://Enemies/Scripts/AI Melee/RunState.gd"),
+	"Attack": preload("res://Enemies/Scripts/AI Melee/AttackState.gd"),
+	"Death": preload("res://Enemies/Scripts/AI Melee/DeathState.gd") ,
+}
+
+func _ready() -> void:
+	change_state("Idle")
+	
+
+func change_state(new_state:String): 
+	var state_temp = state[new_state].new()
+	state_temp.name = new_state
+	add_child(state_temp)
