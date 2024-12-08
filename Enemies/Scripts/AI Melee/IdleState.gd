9b56d467cbb597cf46ec3d5f9ac1bb5ff4f89c92
Skiviper-AJM <adam.mcardlesep13@gmail.com>
2024-12-09 00:00:19 +1300

    Animation tree functions
    Enemy can detect and pathfind to player, just needs to move along that path

--- Enemies/Scripts/AI Melee/IdleState.gd
+++ Enemies/Scripts/AI Melee/IdleState.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 	AIController = get_parent().get_parent()
 	if AIController.Awakening:
 		await AIController.get_node("AnimationTree").finished_animation
-	await AIController.get_node("AnimationTree").get("parameters/playback").travel("Idle")
+	AIController.get_node("AnimationTree").get("parameters/playback").travel("Idle")
 	
 func _physics_process(delta: float) -> void:
 	if AIController:
