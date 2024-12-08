2f1d15e544d1ab16350e222db6e61d708bdf88c5
Skiviper-AJM <adam.mcardlesep13@gmail.com>
2024-12-09 05:41:18 +1300

    fixed issue with hp not resetting between deaths

--- UI/Scripts/Game Over Overlay.gd
+++ UI/Scripts/Game Over Overlay.gd
@@ -7,6 +7,7 @@ func _ready():
 	self.hide()
 
 func _on_retry_pressed() -> void:
+	PlayerData.player_health = PlayerData.player_max_health
 	get_tree().paused = false
 	get_tree().reload_current_scene()
 	
