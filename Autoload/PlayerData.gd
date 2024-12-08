b13cfe0ad3e6220679a483d9423dd26240595e66
Skiviper-AJM <adam.mcardlesep13@gmail.com>
2024-12-09 03:39:29 +1300

    Can drink potions from inventory

--- Autoload/PlayerData.gd
+++ Autoload/PlayerData.gd
@@ -1,3 +1,11 @@
 extends Node
 
 var player_health: int = 10
+var player_max_health: int = 10
+
+var main_hand_equipped: ItemData
+
+func heal_player(amount):
+	player_health += amount
+	if player_health > player_max_health:
+		player_health = player_max_health #prevents overheal
--- UI/Scripts/inventory_slot.gd
+++ UI/Scripts/inventory_slot.gd
@@ -31,7 +31,24 @@ func _drop_data(at_position: Vector2, data: Variant) -> void:
 	data.reparent(self) 
 
 func _physics_process(delta: float) -> void:
-	pass
+	if get_child_count() > 0:
+		var item := get_child(0)
+		
+		#this is how you set the equipped items, can scale to other slots
+		if type == ItemData.Type.WEAPON:
+			PlayerData.main_hand_equipped = item.data
+		else:
+			PlayerData.main_hand_equipped =load("res://UI/Inventory/Item Resources/default_sword.tres")
+	
 
 func _gui_input(event: InputEvent) -> void:
-	pass
+	if event is InputEventMouseButton:
+		if Input.is_action_pressed("Block"):
+			if get_child_count() > 0:
+				#drink healing potion if you clicked it
+				if (get_child(0).data.type == ItemData.Type.HPOTION):
+					PlayerData.heal_player(get_child(0).data.item_health) #heals player by value of used healing flask when right clicked
+					get_child(0).data.count -= 1
+					get_child(0).get_child(0).text = str(get_child(0).data.count)
+					if get_child(0).data.count <= 0:
+						get_child(0).queue_free()
