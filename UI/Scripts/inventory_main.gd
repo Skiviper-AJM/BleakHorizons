94d175588b5e72269b3025931430c6294202b2af
Skiviper-AJM <adam.mcardlesep13@gmail.com>
2024-12-09 03:14:13 +1300

    Working inventory with draggable items

--- UI/Scripts/inventory_main.gd
+++ UI/Scripts/inventory_main.gd
@@ -1,8 +1,21 @@
 extends Control
 
 
+
+var items_to_load = [
+	"res://UI/Inventory/Item Resources/small potion.tres",
+	"res://UI/Inventory/Item Resources/long Sword.tres"
+]
+
+@onready var grid = get_node("grid")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var slot = InventorySlot.new()
-	add_child(slot)
-
+	for i in range(24):
+		var slot = InventorySlot.new()
+		slot.init(ItemData.Type.MAIN, Vector2(32,32))
+		get_node("grid").add_child(slot)
+	for i in items_to_load.size():
+		var item = InventoryItem.new()
+		item.init(load(items_to_load[i]))
+		grid.get_child(i).add_child(item)
