d0579dbca814d8f4fe20f07b05f9ef006f5f554c
Skiviper-AJM <adam.mcardlesep13@gmail.com>
2024-12-09 04:31:38 +1300

    can pick up items and add them to inventory

--- UI/Scripts/inventory_main.gd
+++ UI/Scripts/inventory_main.gd
@@ -15,23 +15,27 @@ func _ready() -> void:
 	add_item("body armor")
 
 
-func add_item(item_name:String) -> void:
+func add_item(item_name: String) -> void:
 	var item = InventoryItem.new()
 	item.init(PlayerData.items[item_name])
 	if item.data.stackable:
+		# Check if we already have the item
 		for i in inventory_size:
-			if grid.get_child(0).get_child_count() > 0:
+			if grid.get_child(i).get_child_count() > 0:
+				# This means the slot has an item, now we check if the item is the same
 				if grid.get_child(i).get_child(0).data == item.data:
-					#updates counter
+					# Add to data count
 					grid.get_child(i).get_child(0).data.count += 1
-					#label counter
+					# Update label counter
 					grid.get_child(i).get_child(0).get_child(0).text = str(grid.get_child(i).get_child(0).data.count)
 					break
 			else:
+				#couldnt find item in inventory, so we create it. only stackable items
 				grid.get_child(i).add_child(item)
 				break
 	else:
+		# Find empty slot
 		for i in inventory_size:
-			if grid.get_child(0).get_child_count() == 0:
+			if grid.get_child(i).get_child_count() == 0:
 				grid.get_child(i).add_child(item)
 				break
---
+++ UI/Scripts/item_object.gd
@@ -0,0 +1,31 @@
+extends Area3D
+
+
+var rng: int
+
+func _ready():
+	var tween = create_tween()
+	var rng_position = Vector3(randi_range(-1,1), 1, randi_range(-1,1))
+	tween.tween_property(self, "position", position + rng_position, 0.2)
+	rng = randi_range(0,1)
+	
+	match rng:
+		0:
+			get_node("long sword").show()
+			get_node("HPotion").hide()
+		1:
+			get_node("long sword").hide()
+			get_node("HPotion").show()
+			
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		self.hide()
+		match rng:
+			0:
+				get_node("../../GUI/Inventory").add_item("long sword")
+			1:
+				get_node("../../GUI/Inventory").add_item("small potion")
+
+
+func _on_pickup_finished():
+	self.queue_free()
